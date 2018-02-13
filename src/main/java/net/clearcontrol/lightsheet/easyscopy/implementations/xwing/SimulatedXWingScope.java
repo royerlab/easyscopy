ed535383d7578d4a1fcd9d3147023a0e9a7348a4
haesleinhuepf <rhaase@mpi-cbg.de>
2018-02-13 20:25:48 +0100

    Refactoring: Every simulated scope should have it's own class as there may be more simulated scopes coming in the future

---
+++ src/main/java/net/clearcontrol/lightsheet/easyscopy/implementations/xwing/SimulatedXWingScope.java
@@ -0,0 +1,36 @@
+package net.clearcontrol.lightsheet.easyscopy.implementations.xwing;
+
+import clearcontrol.microscope.lightsheet.simulation.SimulatedLightSheetMicroscope;
+import net.clearcontrol.lightsheet.easyscopy.EasyLightsheetMicroscope;
+import xwing.XWingMicroscope;
+
+/**
+ * Author: Robert Haase (http://haesleinhuepf.net) at MPI CBG (http://mpi-cbg.de)
+ * February 2018
+ */
+public class SimulatedXWingScope extends EasyLightsheetMicroscope
+{
+  public static boolean sUseStages = false;
+
+  private static SimulatedXWingScope sInstance = null;
+  public static SimulatedXWingScope getInstance() {
+    if (sInstance == null) {
+      sInstance = new SimulatedXWingScope();
+    }
+    return sInstance;
+  }
+
+  private XWingMicroscope mXWingMicroscope;
+  private SimulatedXWingScope() {
+    super(new XWingBuilder(true, sUseStages).getXWingMicroscope());
+    mXWingMicroscope = (XWingMicroscope) getLightSheetMicroscope();
+  }
+
+  public static void cleanup() {
+    if (sInstance != null) {
+      sInstance.terminate();
+      sInstance = null;
+    }
+  }
+
+}
