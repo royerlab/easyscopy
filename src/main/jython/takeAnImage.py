03b8cd3ef17e3c431d737814d6c5f2ee4fa992ff
haesleinhuepf <rhaase@mpi-cbg.de>
2018-02-14 00:29:48 +0100

    make the example script great again

--- src/main/jython/takeAnImage.py
+++ src/main/jython/takeAnImage.py
@@ -1,7 +1,7 @@
 from clearcontrol.devices.lasers import LaserDeviceInterface;
 from clearcontrol.microscope.lightsheet.imaging import DirectImage;
 from clearcontrol.microscope.lightsheet.imaging import DirectImageStack;
-from net.clearcontrol.lightsheet.easyscopy import EasyScopyUtilities;
+from net.clearcontrol.easyscopy import EasyScopyUtilities;
 from net.clearcontrol.easyscopy.lightsheet.implementations.xwing import SimulatedXWingScope;
 from net.imglib2 import RandomAccessibleInterval;
 from net.imglib2.img.display.imagej import ImageJFunctions;
@@ -13,47 +13,44 @@ lScope = SimulatedXWingScope.getInstance();
 
 # Turn on a laser
 lLaser = lScope.getDevice("Laser", "488");
-lLaser.setTargetPowerInPercent(20);
+lLaser.setTargetPowerInPercent(10);
 lLaser.setLaserOn(True);
 lLaser.setLaserPowerOn(True);
 lLaser.setLaserOn(True);
 lLaser.setLaserPowerOn(True);
 
-lScope.getLightSheetMicroscope().getLightSheet(0).getHeightVariable().set(0);
 
 # Take an image
 lImage = lScope.getDirectImage();
-#lImage.setLightSheetIndex(3);
-lImage.setImageWidth(2048);
-lImage.setImageHeight(512);
+lImage.setImageWidth(1024);
+lImage.setImageHeight(2048);
 lImage.setIlluminationZ(25);
 lImage.setDetectionZ(25);
-lImage.setExposureTimeInSeconds(1.0);
 
 # start acquisition
 img = EasyScopyUtilities.stackToImg(lImage.acquire());
 
 # show the images
 ImageJFunctions.show(img);
-
+IJ.run("Enhance Contrast", "saturated=0.35");
 
 
 # take an imagestack
 lImageStack = lScope.getDirectImageStack();
-lImageStack.setImageWidth(2048);
-lImageStack.setImageHeight(512);
+lImageStack.setImageWidth(1024);
+lImageStack.setImageHeight(2048);
 lImageStack.setIlluminationZ(25);
 lImageStack.setDetectionZ(25);
 lImageStack.setNumberOfRequestedImages(10);
-lImageStack.setDetectionZStepDistance(0);
+lImageStack.setDetectionZStepDistance(1);
 lImageStack.setIlluminationZStepDistance(1);
-lImageStack.setExposureTimeInSeconds(1.0);
 
 # start acquisition
 imgStack = EasyScopyUtilities.stackToImg(lImageStack.acquire());
 
 # show the images
 ImageJFunctions.show(imgStack);
+IJ.run("Enhance Contrast", "saturated=0.35");
 
 
 # That's always a godd idea by the end!
