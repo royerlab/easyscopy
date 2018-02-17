54c787356e987fc489a1ab4b1dc86a821716ca94
haesleinhuepf <rhaase@mpi-cbg.de>
2018-02-17 19:23:47 +0100

    updated example jython script to new API

--- src/main/jython/clDownsampling.py
+++ src/main/jython/clDownsampling.py
@@ -1,29 +1,26 @@
 
-from clearcontrol.stack import OffHeapPlanarStack;
-from clearcontrol.stack.imglib2 import ImgToStackConverter;
-from clearcontrol.stack.imglib2 import StackToImgConverter;
 from fastfuse.tasks import DownsampleXYbyHalfTask;
-from ij import IJ;
-from ij import ImageJ;
-from ij import ImagePlus;
-from net.clearcontrol.easyscopy.lightsheet.implementations.clearcl import ClearCLScope;
+from clearcl.imagej import ClearCLIJ;
 from net.imglib2 import RandomAccessibleInterval;
 from net.imglib2.img.display.imagej import ImageJFunctions;
 from net.imglib2.type.numeric.integer import UnsignedShortType;
 from net.imglib2.view import Views;
+from ij import IJ;
 
-# This line takes the current image which is open in ImageJ; it should be a 16-bit unsigned short image
-#@Img lInputImg
+# take the current image which is open in ImageJ
+lImagePlus = IJ.getImage()
 
-# initialize ClearCL context and fastfuse engine
-lScope = ClearCLScope.getInstance();
+# initialize ClearCL context and convenience layer
+lCLIJ = ClearCLIJ.getInstance();
 
 # convert imglib2 image to clearcontrol stack
-lStack = ImgToStackConverter(Views.iterable(lInputImg)).getOffHeapPlanarStack();
+lInputCLImage = lCLIJ.converter(lImagePlus).getOffHeapPlanarStack();
+
+lOutputCLImage = lCLIJ.converter(lImagePlus).getOffHeapPlanarStack();
 
 # downsample the image stack using ClearCL / OpenCL
-lResultStack = lScope.executeUnaryFunction(DownsampleXYbyHalfTask, "kernels/downsampling.cl", "downsample_xy_by_half_nearest", "src", "dst", lStack, None);
+lResultStack = lCLIJ.execute(DownsampleXYbyHalfTask, "kernels/downsampling.cl", "downsample_xy_by_half_nearest", {"src":lInputCLImage, "dst":lOutputCLImage});
 
 # convert the result back to imglib2 and show it
-lResultImg = StackToImgConverter(lResultStack).getRandomAccessibleInterval();
+lResultImg = lCLIJ.converter(lResultStack).getRandomAccessibleInterval();
 ImageJFunctions.show(lResultImg);
