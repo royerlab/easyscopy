7f754163e4f559a579ccf51ebee806d4fb09f37f
debayan102 <debayansaha102@gmail.com>
2018-03-15 18:19:34 +0100

    Queue without camera

--- src/main/java/net/clearcontrol/easyscopy/lightsheet/implementations/blindbscope/demo/BlindBScopeDemo.java
+++ src/main/java/net/clearcontrol/easyscopy/lightsheet/implementations/blindbscope/demo/BlindBScopeDemo.java
@@ -1,8 +1,11 @@
 package net.clearcontrol.easyscopy.lightsheet.implementations.blindbscope.demo;
 
+import clearcontrol.devices.cameras.StackCameraDeviceInterface;
 import clearcontrol.devices.lasers.LaserDeviceInterface;
 import clearcontrol.devices.optomech.filterwheels.FilterWheelDeviceInterface;
 import clearcontrol.devices.slm.slms.DeformableMirrorDevice;
+import clearcontrol.microscope.lightsheet.LightSheetMicroscopeInterface;
+import clearcontrol.microscope.lightsheet.LightSheetMicroscopeQueue;
 import clearcontrol.microscope.lightsheet.spatialphasemodulation.io.DenseMatrix64FReader;
 import clearcontrol.microscope.lightsheet.spatialphasemodulation.slms.SpatialPhaseModulatorDeviceInterface;
 import clearcontrol.microscope.lightsheet.spatialphasemodulation.zernike.TransformMatrices;
@@ -20,7 +23,11 @@ import net.imglib2.img.display.imagej.ImageJFunctions;
 import net.imglib2.type.numeric.integer.UnsignedShortType;
 import org.ejml.data.DenseMatrix64F;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
 import java.io.File;
+import java.util.concurrent.TimeoutException;
 
 /**
  * This demo shows how to initialize two scopes: A BScope without camera and another
@@ -38,13 +45,81 @@ public class BlindBScopeDemo {
 
 
         // --------------------------------------------
+        // Parameters
+
+        int lImageWidth = 2048;
+        int lImageHeight = 2048;
+
+        int lIlluminationZStart = 200;
+        int lDetectionZZStart = 200;
+
+        int llightsheet = 0;
+        double lLightsheetWidth = 0.45;
+        double lLightsheetHeight = 0.0;
+        double lLightsheetX = 0;
+        double lLightsheetY = 0;
+
+        int lDetectionArmIndex = 0;
+
+        // ----------------------------------------------
         // run the BScope without camera
         BlindBScope lScope = BlindBScope.getInstance();
         //System.out.println(lScope.getDevices());
+        LightSheetMicroscopeInterface lLightsheetMicroscope = lScope.getLightSheetMicroscope();
+        //-----------------------------------------------------------------------------------------------------------------------
+        // build a queue
+        LightSheetMicroscopeQueue lQueue = lLightsheetMicroscope.requestQueue();
+        //-----------------------------------------------------------------------------------------------------------------------
+        // initialize queue
+        lQueue.clearQueue();
+        lQueue.setCenteredROI(lImageWidth, lImageHeight);
 
+        lQueue.setI(llightsheet, false);
+        lQueue.setIW(llightsheet, lLightsheetWidth);
+        lQueue.setIH(llightsheet, lLightsheetHeight);
+        lQueue.setIX(llightsheet, lLightsheetX);
+        lQueue.setIY(llightsheet, lLightsheetY);
+
+        lQueue.setIZ(lIlluminationZStart);
+        lQueue.setDZ(lDetectionArmIndex, lDetectionZZStart);
+        //lQueue.setC(lDetectionArmIndex, false);
+
+        lQueue.addCurrentStateToQueue();
+        //lQueue.addCurrentStateToQueue();
+
+        //--------------------------------------------------------------
+        // Acquiring
+        lQueue.setExp(0.001);
+        lQueue.setI(llightsheet, true);
+        lQueue.setIW(llightsheet, lLightsheetWidth);
+        lQueue.setIH(llightsheet, lLightsheetHeight);
+        lQueue.setIX(llightsheet, lLightsheetX);
+        lQueue.setIY(llightsheet, lLightsheetY);
+
+        lQueue.setIZ(lIlluminationZStart);
+        lQueue.setDZ(lDetectionArmIndex, lDetectionZZStart);
+        //lQueue.setC(lDetectionArmIndex, true);
+
+        lQueue.addCurrentStateToQueue();
+
+        //-----------------------------------------------------------------
+        //Closing
+
+        lQueue.setI(llightsheet, false);
+        //lQueue.setC(lDetectionArmIndex, false);
+
+        lQueue.addCurrentStateToQueue();
+
+
+        lQueue.finalizeQueue();
+
+        //-----------------------------------------------------------------
+
+        //Setting Filter Wheel Position
         FilterWheelDeviceInterface lfilter = lScope.getFilterWheelDevice("FLIFilterWheel");
         lfilter.setPosition(7);
 
+        //Turning Laser on
         LaserDeviceInterface lLaser = lScope.getLaserDevice("488");
         lLaser.setTargetPowerInPercent(10);
         lLaser.setLaserOn(true);
@@ -60,55 +135,78 @@ public class BlindBScopeDemo {
         lCamera.setImageWidth(2048);
         lCamera.setExposureTimeInSeconds(0.05);
 
+
+        // ----------------------------------------------
+        boolean lPlayQueueAndWait = false;
+        try {
+            lPlayQueueAndWait = lLightsheetMicroscope.playQueueAndWaitForStacks(lQueue, 10000 + lQueue.getQueueLength(), TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (TimeoutException e) {
+            e.printStackTrace();
+        }
+
+        if (!lPlayQueueAndWait){
+            System.out.println("Error while imaging");}
+
+        // --------------------------------------------------------------------
+        // convert and show
+        //EasyScopyUtilities imgStack = EasyScopyUtilities.stackToImg(lStack);
+        //ImageJFunctions.show(EasyScopyUtilities.stackToImg(lStack));
+        //IJ.run("Enhance Contrast", "saturated=0.35");
+
+
         // --------------------------------------------
         // take a first image
         StackInterface lStack = lCamera.acquire();
 
         RandomAccessibleInterval<UnsignedShortType>
-                img = EasyScopyUtilities.stackToImg((OffHeapPlanarStack)lStack);
+               img = EasyScopyUtilities.stackToImg((OffHeapPlanarStack)lStack);
 
         ImageJFunctions.show(img);
         IJ.run("Enhance Contrast", "saturated=0.35");
 
         // --------------------------------------------
         // Configure the deformable mirror
-        DenseMatrix64F lMatrix = new DenseMatrix64F(11,11);
-        File lMatrixFile = new File("C:\\Users\\myerslab\\.clearcontrol\\MirrorModes\\Neuronal_flat_deb.json");
+        //DenseMatrix64F lMatrix = new DenseMatrix64F(11,11);
+        //File lMatrixFile = new File("C:\\Users\\myerslab\\.clearcontrol\\MirrorModes\\Neuronal_flat_deb.json");
 
-        DenseMatrix64FReader lMatrixReader = new DenseMatrix64FReader(lMatrixFile, lMatrix);
-        lMatrixReader.read();
+        //DenseMatrix64FReader lMatrixReader = new DenseMatrix64FReader(lMatrixFile, lMatrix);
+        //lMatrixReader.read();
 
-        SpatialPhaseModulatorDeviceInterface lMirror = lScope.getSpatialPhaseModulatorDevice("BIL118");
-        lMirror.getMatrixReference().set(lMatrix);
+        //SpatialPhaseModulatorDeviceInterface lMirror = lScope.getSpatialPhaseModulatorDevice("BIL118");
+        //lMirror.getMatrixReference().set(lMatrix);
 
         // --------------------------------------------
         // Take another image
-        lStack = lCamera.acquire();
-        img = EasyScopyUtilities.stackToImg((OffHeapPlanarStack)lStack);
+        //lStack = lCamera.acquire();
+        //img = EasyScopyUtilities.stackToImg((OffHeapPlanarStack)lStack);
 
-        ImageJFunctions.show(img);
-        IJ.run("Enhance Contrast", "saturated=0.35");
+        //ImageJFunctions.show(img);
+        //IJ.run("Enhance Contrast", "saturated=0.35");
 
         // --------------------------------------------
         // some more spatial phase modulation code snippets; to be extended
 
         // Get a matrix with a certain Zernike mode
-        ZernikePolynomialsDenseMatrix64F lZernikeModeMatrix20 = new ZernikePolynomialsDenseMatrix64F(11, 11, 2, 0);
+        //ZernikePolynomialsDenseMatrix64F lZernikeModeMatrix20 = new ZernikePolynomialsDenseMatrix64F(11, 11, 2, 0);
 
         // Sum two matrices
-        DenseMatrix64F lSumMatrix = TransformMatrices.sum(lZernikeModeMatrix20, lMatrix);
+        //DenseMatrix64F lSumMatrix = TransformMatrices.sum(lZernikeModeMatrix20, lMatrix);
 
         // Multiply an matrix with a scalar
-        DenseMatrix64F lMultipliedMatrix = TransformMatrices.multiply(lSumMatrix, 0.5);
+        //DenseMatrix64F lMultipliedMatrix = TransformMatrices.multiply(lSumMatrix, 0.5);
 
         // change a single matrix entry
-        double value = lMultipliedMatrix.get(5,6);
-        value = value + 0.1;
-        lMultipliedMatrix.set(5, 6, value);
+        //double value = lMultipliedMatrix.get(5,6);
+        //value = value + 0.1;
+        //lMultipliedMatrix.set(5, 6, value);
 
         // rotate a matrix
-        DenseMatrix64F lRotatedMatrix = lMatrix.copy();
-        TransformMatrices.rotateClockwise(lMatrix, lRotatedMatrix);
+        //DenseMatrix64F lRotatedMatrix = lMatrix.copy();
+        //TransformMatrices.rotateClockwise(lMatrix, lRotatedMatrix);
 
 
         // --------------------------------------------
